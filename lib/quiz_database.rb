0848545b1d7a31aebc76b57bbe480babf40cdc77
shirotamaki <shiro.tamaki63@gmail.com>
2022-12-11 19:55:34 +0900

    データベースを作成した

---
+++ lib/quiz_database.rb
@@ -0,0 +1,13 @@
+class Quiz
+  def self.quizzes
+    [
+      {
+        question: '角谷さんが在籍していた会社は？',
+        choices: ['株式会社永和システムマネジメント', 'GMOペパボ株式会社', '有限会社アジャイルサムライ'],
+        correct_answer: '株式会社永和システムマネジメント',
+        comment: '正解！',
+        false_comment: '残念！'
+      }
+    ]
+  end
+end
