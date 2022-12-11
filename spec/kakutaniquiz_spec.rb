ec87f6ab196059b3876e4c849fe9f62db6793011
shirotamaki <shiro.tamaki63@gmail.com>
2022-12-11 23:47:07 +0900

    テストファイルを準備

--- spec/kakutaniquiz_spec.rb
+++ spec/kakutaniquiz_spec.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+require_relative '../lib/kakutaniquiz'
+
 RSpec.describe Kakutaniquiz do
   it "has a version number" do
     expect(Kakutaniquiz::VERSION).not_to be nil
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
   end
 end
