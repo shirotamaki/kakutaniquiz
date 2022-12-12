a708d434ec06650f8087d72fb84de26c87b775ed
shirotamaki <shiro.tamaki63@gmail.com>
2022-12-12 22:54:10 +0900

    クイズを追加

--- lib/quiz_database.rb
+++ lib/quiz_database.rb
@@ -2,33 +2,47 @@ class Quiz
   def self.quizzes
     [
       {
-        question: '角谷さんが在籍していた会社は？',
+        question: 'kakutaniさんが顧問として在籍している会社は？',
         choices: ['株式会社永和システムマネジメント', 'GMOペパボ株式会社', 'クックパッド株式会社'],
         correct_answer: '株式会社永和システムマネジメント',
         comment: '正解！フィヨルドブートキャンプの卒業生も多く在籍しているよ♪',
         false_comment: '残念！正解は「株式会社永和システムマネジメント」だよ！'
       },
       {
-        question: '角谷さんが関わっている作品は次の内はどれ？',
+        question: 'kakutaniさんが監訳で関わっている作品は？',
         choices: ['ラストサムライ', 'アジャイルサムライ', 'サムライチャンプルー'],
         correct_answer: 'アジャイルサムライ',
-        comment: '正解！',
+        comment: '正解！フィヨルドブートキャンプの参考書籍だよ',
         false_comment: '残念！正解は「アジャイルサムライ」だよ！'
       },
       {
-        question: '角谷さんの好きな効果音は？',
+        question: 'kakutaniさんが好きなRubyのメソッドは？',
+        choices: ['Object#extend', 'Module#include', 'Enumerable#map'],
+        correct_answer: 'Object#extend',
+        comment: '正解！',
+        false_comment: '残念！正解は「Object#extend」だよ！'
+      },
+      {
+        question: 'kakutaniさんが最近ハマっている好きなお酒の銘柄は？',
+        choices: ['白玉醸造の魔王', '久米島の久米仙', '豊の秋のミドリ'],
+        correct_answer: '豊の秋のミドリ',
+        comment: '正解！',
+        false_comment: '残念！正解は「豊の秋のミドリ」だよ！'
+      },
+      {
+        question: 'kakutaniさんの好きな効果音は？',
         choices: ['ズキューン', 'メメタァ', 'ドギャーン'],
         correct_answer: 'ドギャーン',
         comment: '正解！あと「ゴゴゴゴゴ」も好きだよ',
         false_comment: '残念！正解は「ドギャーン」だよ！'
       },
       {
-        question: '角田さんが2012年のEuruko会場前の屋台で、Matzからご馳走してもらった食べ物とは？',
+        question: 'kakutaniさんが2012年のEuruko会場前の屋台で、Matzからご馳走してもらった食べ物とは？',
         choices: ['コロッケ', 'ホットドック', 'ワッフル'],
         correct_answer: 'コロッケ',
-        comment: '正解！この出来事は後に「アムスコロッケ買収事件」と呼ばれるよ',
-        false_comment: '残念！正解はコロッケです！この出来事は後に「アムスコロッケ買収事件」と呼ばれるよ'
-      },
+        comment: '正解！この出来事は、後に「アムスコロッケ買収事件」と呼ばれるよ',
+        false_comment: '残念！正解はコロッケだよ！この出来事は、後に「アムスコロッケ買収事件」と呼ばれるよ'
+      }
     ]
   end
 end
